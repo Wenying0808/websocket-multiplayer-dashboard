953cee046891bbc4762478ea9f9b5df0ee4e2858
Wenying Chang <changwenyingnl@gmail.com>
2025-02-05 20:45:04 +0100

    feat:/add chatform component

--- real-time-chat/app/page.tsx
+++ real-time-chat/app/page.tsx
@@ -1,11 +1,14 @@
+import ChatForm from "@/components/ChatForm";
+
 export default function Home() {
   return (
     <div className="flex mt-24 justify-center w-full">
       <div className="w-full max-w-3xl mx-auto">
         <h1 className="mb-4 text-2xl font-bold">Room:</h1>
         <div>
-          TODO:
+          TODO: Add chat room
         </div>
+        <ChatForm/>
       </div>
     </div>
   );
