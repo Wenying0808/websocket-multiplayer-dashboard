b5ad8c5cf6460def8739ccfbb4893ab13b422c31
Wenying Chang <changwenyingnl@gmail.com>
2025-02-05 20:38:07 +0100

    create nextjs project

--- client/index.js
+++
@@ -1,5 +0,0 @@
-const socket = io("http://localhost:3100");
-
-socket.on("connect", (response) => {
-    console.log(response);
-})
---
+++ real-time-chat/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="flex mt-24 justify-center w-full">
+      <div className="w-full max-w-3xl mx-auto">
+        <h1 className="mb-4 text-2xl font-bold">Room:</h1>
+        <div>
+          TODO:
+        </div>
+      </div>
+    </div>
+  );
+}
