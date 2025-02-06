0d2c6fe142128ae5624cfdf1740be3b9d81b61eb
Wenying Chang <changwenyingnl@gmail.com>
2025-02-06 15:38:13 +0100

    feat:/polish charform component

--- real-time-chat/components/ChatForm.tsx
+++ real-time-chat/components/ChatForm.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
+"use client";
 
-const ChatForm = () => {
-  return (
-    <div>
-      chatform
-    </div>
-  )
+import React, { useState } from 'react'
+
+type ChatFormProps = {
+    onSendMessage: (message: string) => void;
 }
+
+const ChatForm: React.FC<ChatFormProps> = ({ onSendMessage }) => {
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        // empty the input after submit
+        if(message.trim() !== ""){
+            onSendMessage(message);
+            setMessage("");
+        }
+        console.log("submitted")
+    }
+  return (
+    <form onSubmit={handleSubmit} className='flex gap-2 mt-4'>
+        <input 
+            type="text" 
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder='Type your message here' 
+            className='flex-1 px-4 py-2 rounded-lg border-2 focus:outline-none'
+        />
+        <button type="submit" className="px-4 py-2 text-white bg-blue-500 rounded-lg">Send</button>
+    </form>
+  );
+};
 
 export default ChatForm
